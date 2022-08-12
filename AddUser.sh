0305ea313e171a672a748802bd736d1f796bc939
ex0therm <81408590+ex0th@users.noreply.github.com>
2022-08-12 18:00:29 +0200

    Create AddUser.sh

---
+++ AddUser.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+true
+SCRIPT_NAME="Add CLI User"
+# shellcheck source=lib.sh
+source /var/scripts/lib.sh
+
+
+if [[ $UNIXUSER != "veeam" ]]
+then
+    msg_box "Current user with sudo permissions is: $UNIXUSER.
+This script will set up everything with that user.
+If the field after ':' is blank you are probably running as a pure root user.
+It's possible to install with root, but there will be minor errors.
+Please create a user with sudo permissions if you want an optimal installation.
+The preferred user is 'ncadmin'."
+    if ! yesno_box_yes "Do you want to create a new user?"
+    then
+        print_text_in_color "$ICyan" "Not adding another user..."
+        sleep 1
+    else
+        read -r -p "Enter name of the new user: " NEWUSER
+        adduser --disabled-password --gecos "" "$NEWUSER"
+        sudo usermod -aG sudo "$NEWUSER"
+        usermod -s /bin/bash "$NEWUSER"
+        while :
+        do
+            sudo passwd "$NEWUSER" && break
+        done
+        sudo -u "$NEWUSER" sudo bash "$1"
+    fi
+fi
