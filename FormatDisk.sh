5ecd40a5bf8c555bd442201b3400373b93770cdb
ex0sandata <108812382+ex0sandata@users.noreply.github.com>
2022-08-12 13:48:29 +0200

    Update FormatDisk.sh

--- FormatDisk.sh
+++ FormatDisk.sh
@@ -4,7 +4,8 @@
 #### Einige Teile dieses Scripts stammen von hier: https://github.com/nextcloud/vm/blob/master/nextcloud_install_production.sh/ ####
 
 true
-SCRIPT_NAME="Format Chosen Disk"
+SCRIPT_NAME="Festplatte Formatieren"
+MOUNT_="/opt/backups"
 # shellcheck source=lib.sh
 source /var/scripts/fetch_lib.sh
 
@@ -21,63 +22,8 @@ umount /mnt/* &> /dev/null
 # mkdir if not existing
 mkdir -p "$MOUNT_"
 
-# Check what Hypervisor disks are available
-if [ "$SYSVENDOR" == "VMware, Inc." ];
-then
-    SYSNAME="VMware"
-    DEVTYPE=sdb
-elif [ "$SYSVENDOR" == "Microsoft Corporation" ];
-then
-    SYSNAME="Hyper-V"
-    DEVTYPE=sdb
-elif [ "$SYSVENDOR" == "innotek GmbH" ];
-then
-    SYSNAME="VirtualBox"
-    DEVTYPE=sdb
-elif [ "$SYSVENDOR" == "Xen" ];
-then
-    SYSNAME="Xen/XCP-NG"
-    DEVTYPE=xvdb
-elif [[ "$SYSVENDOR" == "QEMU" || "$SYSVENDOR" == "Red Hat" ]];
-then
-    SYSNAME="KVM/QEMU"
-    DEVTYPE=vdb
-elif [ "$SYSVENDOR" == "DigitalOcean" ];
-then
-    SYSNAME="DigitalOcean"
-    DEVTYPE=sda
-elif [ "$SYSVENDOR" == "Intel(R) Client Systems" ];
-then
-    SYSNAME="Intel-NUC"
-    DEVTYPE=sda
-elif [ "$SYSVENDOR" == "UpCloud" ];
-then
-    if lsblk -e7 -e11 | grep -q sd
-    then
-        SYSNAME="UpCloud ISCSI/IDE"
-        DEVTYPE=sdb
-    elif lsblk -e7 -e11 | grep -q vd
-    then
-        SYSNAME="UpCloud VirtiO"
-        DEVTYPE=vdb
-    fi
-elif partprobe /dev/sdb &>/dev/null;
-then
-    SYSNAME="machines"
-    DEVTYPE=sdb
-elif partprobe /dev/nvme0n1  &>/dev/null;
-then
-    SYSNAME="NVMe"
-    DEVTYPE=nvme0n1
-else
-    msg_box "It seems like you didn't add a second disk. 
-To be able to put the DATA on a second drive formatted as ZFS you need to add a second disk to this server.
-This script will now exit. Please add a second disk and start over."
-    exit 1
-fi
-
-msg_box "You will now see a list with available devices. Choose the device where you want to put your Nextcloud data.
-Attention, the selected device will be formatted!"
+msg_box "Sie werden nun eine Liste von Geräten sehen, auf welchen das Backupverzeichnis angelegt werden soll:
+Achtung! Alle Daten auf dieser Platte werden gelöscht werden!"
 AVAILABLEDEVICES="$(lsblk | grep 'disk' | awk '{print $1}')"
 # https://github.com/koalaman/shellcheck/wiki/SC2206
 mapfile -t AVAILABLEDEVICES <<< "$AVAILABLEDEVICES"
@@ -101,65 +47,56 @@ done
 DISKTYPE=$(fdisk -l | grep "$DEVTYPE" | awk '{print $2}' | cut -d ":" -f1 | head -1)
 if [ "$DISKTYPE" != "/dev/$DEVTYPE" ]
 then
-    msg_box "It seems like your $SYSNAME secondary volume (/dev/$DEVTYPE) does not exist.
-This script requires that you mount a second drive to hold the data.
-Please shutdown the server and mount a second drive, then start this script again.
-If you want help you can buy support in our shop:
-https://shop.hanssonit.se/product/premium-support-per-30-minutes/"
+    msg_box "Es scheint, als würde auf diesem Rechner kein 2. Datenträger (/dev/$DEVTYPE) existieren.
+Dieses Skript setzt eine 2. Festplatte voraus.
+Bitte fahren Sie diesen Server wieder herunter und installieren eine zusätzliche Festplatte."
     exit 1
 fi
 
-# Check if ZFS utils are installed
-install_if_not zfsutils-linux 
 
 # Check still not mounted
 #These functions return exit codes: 0 = found, 1 = not found
 isMounted() { findmnt -rno SOURCE,TARGET "$1" >/dev/null;} #path or device
 isDevMounted() { findmnt -rno SOURCE        "$1" >/dev/null;} #device only
 isPathMounted() { findmnt -rno        TARGET "$1" >/dev/null;} #path   only
-isDevPartOfZFS() { zpool status | grep "$1" >/dev/null;} #device member of a zpool
 
-if isPathMounted "/mnt/ncdata";      #Spaces in path names are ok.
+if isPathMounted "/opt/backups";      #Spaces in path names are ok.
 then
-    msg_box "/mnt/ncdata is mounted and need to be unmounted before you can run this script."
+    msg_box "/opt/backups ist im Moment gemountet und muss unmountet werden, um dieses Skript auszuführen."
     exit 1
 fi
 
 if isDevMounted "/dev/$DEVTYPE";
 then
-    msg_box "/dev/$DEVTYPE is mounted and need to be unmounted before you can run this script."
+    msg_box "/dev/$DEVTYPE ist im Moment gemountet und muss unmountet werden, um dieses Skript auszuführen."
     exit 1
 fi
 
 # Universal:
-if isMounted "/mnt/ncdata";
+if isMounted "/opt/backups";
 then
-    msg_box "/mnt/ncdata is mounted and need to be unmounted before you can run this script."
+    msg_box "/opt/backups ist im Moment gemountet und muss unmountet werden, um dieses Skript auszuführen."
     exit 1
 fi
 
 if isMounted "/dev/${DEVTYPE}1";
 then
-    msg_box "/dev/${DEVTYPE}1 is mounted and need to be unmounted before you can run this script."
+    msg_box "/dev/${DEVTYPE}1 ist im Moment gemountet und muss unmountet werden, um dieses Skript auszuführen."
     exit 1
 fi
 
-if isDevPartOfZFS "$DEVTYPE";
-then
-    msg_box "/dev/$DEVTYPE is a member of a ZFS pool and needs to be removed from any zpool before you can run this script."
-    exit 1
-fi
 
 if lsblk -l -n | grep -v mmcblk | grep disk | awk '{ print $1 }' | tail -1 > /dev/null
 then
-    msg_box "Formatting your $SYSNAME secondary volume ($DISKTYPE) when you hit OK.
-*** WARNING: ALL YOUR DATA WILL BE ERASED! ***"
+    msg_box "Formattiere auf diesem System das Volumen: ($DISKTYPE) wenn Sie OK drücken.
+*** WARNUNG: ALLE DATEN AUF DIESEM DATENTRÄGER WERDEN GELÖSCHT! ***"
     if zpool list | grep "$POOLNAME" > /dev/null
     then
-        check_command zpool destroy "$POOLNAME"
+        wipefs -a -f $DISKTYPE
     fi
-    check_command wipefs -a -f "$DISKTYPE"
+    check_command wipefs -a -f /dev/"$DISKTYPE"
     sleep 0.5
+    mkfs.xfs -b size=4096 -m crc=1,reflink=1 /dev/"$DISKTYPE" -f
     
 
 else
@@ -172,6 +109,7 @@ https://shop.hanssonit.se/product/premium-support-per-30-minutes/"
     exit 1
 fi
 }
+
 format
 
 
