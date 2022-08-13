bd5e432c74e8373d9336219d9b8dadfa0d37a548
ex0therm <81408590+ex0th@users.noreply.github.com>
2022-08-13 16:20:32 +0200

    Update AddUser.sh

--- AddUser.sh
+++ AddUser.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 true
-SCRIPT_NAME="Add CLI User"
+SCRIPT_NAME="Add Backup User"
 # shellcheck source=lib.sh
 source /var/scripts/lib.sh
 
+UNIXUSER=$(whoami)
 
 if [[ $UNIXUSER != "veeam" ]]
 then
-    msg_box "Current user with sudo permissions is: $UNIXUSER.
-This script will set up everything with that user.
-If the field after ':' is blank you are probably running as a pure root user.
-It's possible to install with root, but there will be minor errors.
-Please create a user with sudo permissions if you want an optimal installation.
-The preferred user is 'ncadmin'."
-    if ! yesno_box_yes "Do you want to create a new user?"
+    msg_box "Veeam empfiehlt einen neuen User anzulegen, der keine sudo- Rechte besitzt.
+    Der aktuelle User ist: $UNIXUSER.
+    Dieses Skript erstellt einen neuen User, der auf das Backupverzeichnis zugreifen darf.
+    Der User darf keine sudo- Rechte haben!"
+    if ! yesno_box_yes "Soll ein neuer User erstellt werden?"
     then
         print_text_in_color "$ICyan" "Not adding another user..."
         sleep 1
     else
-        read -r -p "Enter name of the new user: " NEWUSER
-        adduser --disabled-password --gecos "" "$NEWUSER"
-        sudo usermod -aG sudo "$NEWUSER"
-        usermod -s /bin/bash "$NEWUSER"
+        print_text_in_color "$IPurple" "Neuer User wird erstellt:"
+        read -r -p "Namen des neuen Users eingeben: " NEWUSER
+        useradd -b /opt/backups -d /home/backups -s /bin/bash "$NEWUSER"
+        
+        print_text_in_color "$IGreen" "Neuer User mit dem Namen $NEWUSER angelegt"
+        groupadd VeeamBR
+        usermod -G VeeamBR "$NEWUSER"
         while :
         do
             sudo passwd "$NEWUSER" && break
         done
         sudo -u "$NEWUSER" sudo bash "$1"
+
+        print_text_in_color "$IGreen" "Verzeichnis /opt/backups auf $NEWUSER berechtigt"
+        chown -R "$NEWUSER" /opt/backups 
+        chmod 770 /opt/backups
     fi
 fi
