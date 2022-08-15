c6e48ab9b2e2e7d757b1b3ccbd0921c681f949ee
ex0sandata <108812382+ex0sandata@users.noreply.github.com>
2022-08-15 18:53:26 +0200

    Update AddUser.sh

--- AddUser.sh
+++ AddUser.sh
@@ -4,6 +4,8 @@ SCRIPT_NAME="Add Backup User"
 # shellcheck source=lib.sh
 source /var/scripts/lib.sh
 
+
+### Change passwords
 UNIXUSER=$(whoami)
 
 if [[ $UNIXUSER != "veeam" ]]
@@ -12,26 +14,73 @@ then
     Der aktuelle User ist: $UNIXUSER.
     Dieses Skript erstellt einen neuen User, der auf das Backupverzeichnis zugreifen darf.
     Der User darf keine sudo- Rechte haben!"
+
     if ! yesno_box_yes "Soll ein neuer User erstellt werden?"
     then
         print_text_in_color "$ICyan" "Not adding another user..."
         sleep 1
     else
-        print_text_in_color "$IPurple" "Neuer User wird erstellt:"
-        read -r -p "Namen des neuen Users eingeben: " NEWUSER
-        useradd -b /opt/backups -d /home/backups -s /bin/bash "$NEWUSER"
+        useradd veeam
+        useradd -b /opt/backups -d /home/backups -s /bin/bash "$veeam"
         
-        print_text_in_color "$IGreen" "Neuer User mit dem Namen $NEWUSER angelegt"
+        print_text_in_color "$IGreen" "Neuer User mit dem Namen veeam angelegt"
         groupadd VeeamBR
-        usermod -G VeeamBR "$NEWUSER"
+        usermod -G VeeamBR veeam
+        
+        print_text_in_color "$IGreen" "Verzeichnis /opt/backups auf 'veeam' berechtigt"
+        chown -R veeam:VeeamBR /opt/backups 
+        chmod 770 /opt/backups
+
+        msg_box "Bitte wählen Sie ein starkes Passwort für den neuen User 'veeam'"
         while :
         do
-            sudo passwd "$NEWUSER" && break
+            UNIX_PASSWORD=$(input_box_flow "Bitte ein neues Passwort eingeben:")
+            if [[ "$UNIX_PASSWORD" == *" "* ]]
+            then
+                msg_box "Bitte keine Leerzeichen benutzen."
+            else
+                break
+            fi
         done
-        sudo -u "$NEWUSER" sudo bash "$1"
 
-        print_text_in_color "$IGreen" "Verzeichnis /opt/backups auf $NEWUSER berechtigt"
-        chown -R "$NEWUSER":VeeamBR /opt/backups 
-        chmod 770 /opt/backups
+        if echo "veeam:$UNIX_PASSWORD" | sudo chpasswd
+        then
+            msg_box "Das Passwort für den User 'veeam' ist nun '$UNIX_PASSWORD'. Dieses Passwort muss in der Veeam Konsole später eingegeben werden."
+        fi      
+
     fi
 fi
+
+
+msg_box "Für die einmalige Einrichtung dieses Servers in der Veeam Konsole muss das Root Passwort festgelegt werden.
+Dieses MUSS ein starkes Passwort sein!"
+    while :
+    do
+        ROOT_PASSWORD=$(input_box_flow "Bitte ein neues Passwort eingeben:")
+        if [[ "$ROOT_PASSWORD" == *" "* ]]
+        then
+            msg_box "Bitte keine Leerzeichen benutzen."
+        else
+            break
+        fi
+
+        ROOT_PASSWORD=$(input_box_flow "Bitte ein neues Passwort eingeben:")
+        if [[ $(expr length "$ROOT_PASSWORD" -lt 15 ]]
+        then
+            msg_box "Das Passwort muss mindestens 15 Zeichen besitzen."
+        else
+            break
+        fi
+    done
+    
+    if echo "root:$ROOT_PASSWORD" | sudo chpasswd
+    then
+        msg_box "Das Passwort für den User 'root' ist nun '$ROOT_PASSWORD'. Dieses Passwort muss in der Veeam Konsole später eingegeben werden."
+    fi
+
+msg_box "Dieser Server ist nun fertig eingerichtet. Bitte notieren Sie folgende Daten, die Sie in der Veeam Konsole 
+eingeben müssen, um den Server zu verbinden: \
+
+
+Eine Anleitung mit Bildern zur Einrichtung in der Konsole 
+"
