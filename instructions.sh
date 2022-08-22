4a69017aab573600dde22f7fabae3e7350c38b40
ex0sandata <108812382+ex0sandata@users.noreply.github.com>
2022-08-22 10:05:55 +0200

    Update instructions.sh

--- instructions.sh
+++ instructions.sh
@@ -6,15 +6,18 @@ clear
 
 #### Start:
 source /var/scripts/lib.sh
+UNIXUSER=$(whoami)
 
+  msg_box "Willkommen bei der Erstinstallation eines Veeam Hardened Linux Repositories! 
 
-  msg_box " Willkommen bei der Erstinstallation eines Veeam Hardened Linux Repositories! 
 Optional:                                                           
 Sie können den Server auch via SSH-Session einrichten. Windows unterstützt dieses Feature seit v.1809 nativ
 auf Konsolen (cmd / PS). Sie können sich wie folgt verbinden: ssh $UNIXUSER@IP-Adresse"
-    if yesno_box_yes "Fortfahren(y/n)?"
+    if yesno_box_yes "Möchten Sie mit der Einrichtung fortfahren(y/n)?"
     then
         print_text_in_color "$IGreen" "Setup wird gestartet.."
+        msg_box "Alle eigenen Konfigurationen, wie Benutzernamen und Passwörter werden nach der Einrichtung des Veeam Hardened Linux Repositories in der Textdatei
+        /var/scripts/VHLR.txt gespeichert. Diese Credentials sind für die Einrichtung in der Veeam Konsole wichtig."
         print_text_in_color "$IPurple" " ###################### Silas Suessmilch - SanData GmbH - $(date +"%Y") ######################"
     else
         print_text_in_color "$IRed" "Abbruch..."
