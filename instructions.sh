f6e628d93129a21a8790fcd1e60a1ebbb8e823d0
ex0sandata <108812382+ex0sandata@users.noreply.github.com>
2022-08-22 08:25:17 +0200

    Update instructions.sh

--- instructions.sh
+++ instructions.sh
@@ -3,32 +3,22 @@
 # Silas Suessmilch © - 2022
 
 clear
-cat << INST1
-+-----------------------------------------------------------------------+
-|              Willkommen bei der Erstinstallation eines                |
-|                 Veeam Hardened Linux Repositories!                    |
-|                                                                       |
-INST1
-echo "|                                                                       |"
-echo "|                    Um mit der Einrichtung zu starten,                 |"
-echo "|                  bestätigen Sie bitte die Eingabe mit y/n.            |"
-echo "|                                                                       |"
-read -p "|                           Fortfahren(y/n)?                            |" CONT
-echo "|                                                                       |"
-if [ "$CONT" = "y" ]; then
-cat << INST2
-|                                                                       |
-| Optional:                                                             |
-| Sie können den Server auch via SSH-Session einrichten. Windows        |
-| unterstützt dieses Feature seit v.1809 nativ auf Konsolen (cmd / PS)  |
-| Sie können sich wie folgt verbinden: ssh $UNIXUSER@IP-Adresse         |
-|                                                                       |
-|                                                                       |
-|  ###################### Silas Suessmilch - SanData GmbH - $(date +"%Y") ######################  |
-+-----------------------------------------------------------------------+
-INST2
-else
-    exit 1 && break
-fi
+
+#### Start:
+source /var/scripts/lib.sh
+
+
+  msg_box " Willkommen bei der Erstinstallation eines Veeam Hardened Linux Repositories! 
+Optional:                                                           
+Sie können den Server auch via SSH-Session einrichten. Windows unterstützt dieses Feature seit v.1809 nativ
+auf Konsolen (cmd / PS). Sie können sich wie folgt verbinden: ssh $UNIXUSER@IP-Adresse"
+    if yesno_box_yes "Fortfahren(y/n)?"
+    then
+        print_text_in_color "$IGreen" "Setup wird gestartet.."
+        print_text_in_color "$IPurple" " ###################### Silas Suessmilch - SanData GmbH - $(date +"%Y") ######################"
+    else
+        print_text_in_color "$IRed" "Abbruch..."
+        exit 1 && break
+    fi
 
 exit 0
