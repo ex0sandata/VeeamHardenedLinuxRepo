3db19aac09f6f9259e9afe82316d08fa9db0b4f9
ex0sandata <108812382+ex0sandata@users.noreply.github.com>
2022-10-27 14:14:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -57,6 +57,6 @@ curl https://raw.githubusercontent.com/ex0sandata/VeeamHardenedLinuxRepo/main/st
 chmod +x /var/scripts/*.sh
 
 #### Start:
-screen -dm -S "VHLR" /bin/bash /var/scripts/SetupHardenedLinuxRepo.sh && screen -r
+/bin/screen -mS VHLR /bin/bash /var/scripts/SetupHardenedLinuxRepo.sh
 
 #/bin/bash /var/scripts/SetupHardenedLinuxRepo.sh
